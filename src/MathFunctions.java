bd2f8e3d78999f7c2c211a213430d70bb17b3a78
camlo01 <camilobeltran174@gmail.com>
2022-03-30 09:29:12 -0500

    Java Function y Java Predicate

--- src/MathFunctions.java
+++ src/MathFunctions.java
@@ -1,4 +1,8 @@
+import groovyjarjarpicocli.CommandLine;
+import org.codehaus.groovy.runtime.dgmimpl.arrays.BooleanArrayGetAtMetaMethod;
+
 import java.util.function.Function;
+import java.util.function.Predicate;
 
 public class MathFunctions {
 
@@ -9,20 +13,78 @@ public class MathFunctions {
 
 
         //Manera de crear funciones en Java, característica agregada desde Java 8
-    Function<Integer, Integer> squareFunction = new Function<Integer, Integer>() {
-        @Override
-        public Integer apply(Integer x) {
-            return x * x;
-        }
-    };
+        Function<Integer, Integer> squareFunction = new Function<Integer, Integer>() {
+            @Override
+            public Integer apply(Integer x) {
+                return x * x;
+            }
+        };
         System.out.println(squareFunction.apply(5));
         System.out.println(squareFunction.apply(25));
         System.out.println(square(5));
         System.out.println(square(25));
+        //Funcion más corta y relativamente más legible
+
+
+        Function<Integer, Boolean> isOdd = x -> x % 2 == 1;
+
+        //Preicate es un función que trabaja sobre un tipo y genera una función
+        // Prueba si algo es valido, En este caso si un númro es par, y de ser así retorna true
+        // Y estos se validan con el método test();
+        Predicate<Integer> isEven = x -> x % 2 == 0;
+
+
+        System.out.println(isEven.test(4));
+
+//        método que define si un estudiante aprobó si su calificación es mayor a 6.0
+        Predicate<Student> isApproved = student -> student.getCalification() > 6.0;
+
+//        creamos un estudiante por su calificacion
+        Student sinuhe =  new Student(5.9);
+
+//       Imprimimos por pantalla si el estudiante calificó
+        System.out.println(isApproved.test(sinuhe));
+
+    } //FIN MAIN
+
+
+    static class Student{
+//      creamos una clase de estudiante
+        private double calification;
+
+        public Student(double calification) {
+            this.calification = calification;
+        }
+
+        public double getCalification() {
+            return calification;
+        }
     }
-    static int square(int x){
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    //Como se haría normalmente
+    static int square(int x) {
         return x * x;
 
     }
+
 
 }
