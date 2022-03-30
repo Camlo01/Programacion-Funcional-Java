d231896faa07ff139d924534d51346e9fc3bf2a0
camlo01 <camilobeltran174@gmail.com>
2022-03-30 12:06:31 -0500

    podemos crear nuestras propias funciones

---
+++ src/SAM/AgeUtils.java
@@ -0,0 +1,35 @@
+package SAM;
+
+import java.time.LocalDate;
+import java.time.Period;
+import java.util.function.Function;
+
+public class AgeUtils {
+
+    public static void main(String[] args) {
+
+        Function<Integer, String> addCeros = x -> x < 10 ? "0" + x : String.valueOf(x);
+
+
+        TriFunction<Integer, Integer, Integer, LocalDate> parseDate =
+                (day, month, year) -> LocalDate.parse(year + "-" + addCeros.apply(month) + "-" + addCeros.apply(day));
+
+        TriFunction<Integer, Integer, Integer, Integer> calculateAge =
+                (day, month, year) -> Period.between(parseDate.apply(day, month, year), LocalDate.now()).getYears();
+
+
+        System.out.println(calculateAge.apply(24, 7, 2004));
+    }
+
+
+    @FunctionalInterface
+    interface TriFunction<T, U, V, R> {
+        R apply(T t, U u, V v);
+
+    }
+
+    // Que es una
+
+    // SAM son las siglas de Single Abstract Method y representa una interfaz que tiene un solo metodo sin definir
+
+}
